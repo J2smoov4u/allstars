50593c8d77e8badb2215e87f57419867c7d2c268
James McDougald <2smooventertainment@gmail.com>
2017-10-07 17:54:12 +0200

    added main.js and slick carousel.

---
+++ resources/js/main.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+  $('.carousel').slick({
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 9000,
+    arrows: false,
+    draggable: true,
+    pauseOnFocus: false,
+    pauseOnHover: false
+  });
+});
