867f88c668055bc24b5e98fe31ee7fab7a299afb
James McDougald <2smooventertainment@gmail.com>
2017-10-23 16:07:47 +0200

    fixed footer and some picture sizes.

--- resources/js/main.js
+++ resources/js/main.js
@@ -3,7 +3,7 @@ $(document).ready(function(){
   $('.carousel').slick({
     slidesToScroll: 1,
     autoplay: true,
-    autoplaySpeed: 9000,
+    autoplaySpeed: 6000,
     arrows: false,
     draggable: true,
     pauseOnFocus: false,
